d1b1e178e0c3a567a28e359e3bae97cb1594e3fa
Altin <altin.shala@web.de>
2022-07-01 18:49:16 +0200

    Applied first test of photon sphere Current behaviour as expected. Next TODO: Implement events which terminate the integration Next TODO: repeat previous equatorial plane calculation Next TODO: redo meriodinal plane calculation and hope for the best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from initialconditions import lin_comb, space_light_timelike
+from initialconditions import lin_comb, space_light_timelike, check_tetrad
 from metric import q_metric
 from numpy import array, sin, cos, sum, pi, linspace, vstack, sqrt, tan, copy
 from scipy.integrate import solve_ivp, odeint
@@ -36,15 +36,6 @@ def too_deep(t, y):
     return y[1] - 2.5
 
 
-def check_tetrad(init_pos, quad, mass):
-    g = q_metric(init_pos, [quad, mass])
-    triad = [[0, -1 * (1 / sqrt(g[2])), 0],
-                [0, 0, 1 * (1 / sqrt(g[3]))],
-                [-1 * (1 / sqrt(g[1])), 0, 0]
-                ]
-    return triad
-
-
 def plot_data(result_sp, ax):
     r = result_sp[0]
     theta = result_sp[1]
@@ -59,88 +50,11 @@ def plot_data(result_sp, ax):
     ax.set_zlim([-r_anzeige, r_anzeige])
 
 
-def solve_this(metric: str, solver: str, init_pos: array, init_four_vel: array, t_span, mass, quad, energy, lz):
-    """
-    Method to try out other solvers
-    :param metric:
-    :param solver:
-    :param init_pos:
-    :param init_four_vel:
-    :param t_span:
-    :param mass:
-    :param quad:
-    :param energy:
-    :param lz:
-    :return:
-    """
-    u_t, u_r, u_theta, u_phi = init_four_vel
-    tt, rr1, theta_1, phi1 = init_pos
-    if metric == "quadrupol" and solver == "ivp":
-        sol = solve_ivp(quadrupol_ivp,
-                        t_span=t_span,
-                        y0=array([tt, rr1, u_r, theta_1, u_theta, phi1]),
-                        args=(mass, quad, energy, lz),
-                        vectorized=True,
-                        method='RK45',
-                        atol=1e-7,
-                        rtol=1e-7,
-                        dense_output=True
-                        )
-        print(sol)
-        result_spherical = vstack([sol.y[1], sol.y[3], sol.y[5]])
-
-    elif metric == "quadrupol" and solver == "odeint":
-        t_span = linspace(t_span[0], t_span[1], 200)
-        sol = odeint(quadrupol_odeint,
-                     y0=array([tt, rr1, u_r, theta_1, u_theta, phi1]),
-                     t=t_span,
-                     args=(mass, quad, energy, lz),
-                     atol=1e-7,
-                     rtol=1e-7
-                     )
-        result_spherical = vstack([sol[:, 1], sol[:, 3], sol[:, 5]])
-
-    elif metric == "schwarzschild" and solver == "odeint":
-        t_span = linspace(t_span[0], t_span[1], 200)
-        sol = odeint(schwarzschild_odeint,
-                     y0=[tt, u_t, rr1, u_r, theta_1, u_theta, phi1, u_phi],
-                     t=t_span,
-                     atol=1e-4,
-                     rtol=1e-4
-                     )
-        result_spherical = vstack([sol[:, 2], sol[:, 4], sol[:, 6]])
-
-    elif metric == "schwarzschild" and solver == "ivp":
-        sol = solve_ivp(schwarzschild_ivp,
-                        t_span=t_span,
-                        y0=[tt, u_t, rr1, u_r, theta_1, u_theta, phi1, u_phi],
-                        atol=1e-7,
-                        rtol=1e-7,
-                        dense_output=True,
-                        events=too_deep)
-        result_spherical = vstack([sol.y[2], sol.y[4], sol.y[6]])
-
-    else:
-        sol = solve_ivp(quadrupol_ivp,
-                        t_span=t_span,
-                        y0=array([tt, rr1, u_r, theta_1, u_theta, phi1]),
-                        args=(mm, qq, e, angular_mom),
-                        vectorized=True,
-                        method='RK45',
-                        atol=1e-5,
-                        rtol=1e-5,
-                        dense_output=True
-                        )
-        result_spherical = vstack([sol.y[1], sol.y[3], sol.y[5]])
-
-    return result_spherical
-
-
 if __name__ == "__main__":
-    dist = 10
+    dist = 5
     init_ort = [0, dist, pi/2, 0]          # for some reason r phi theta
     mm = 1
-    qq = 0
+    qq = 1
     grid_steps = 1
     lw_theta, hg_theta, lw_phi, hg_phi = 90, 90, 30, 60
     angles = [lw_theta, hg_theta, lw_phi, hg_phi]
@@ -157,18 +71,18 @@ if __name__ == "__main__":
         u_t, u_r, u_theta, u_phi = init_4_vel
         tt, rr1, theta_1, phi1 = init_ort
         sol = solve_ivp(quadrupol_ivp,
-                        t_span=[0,10],
+                        t_span=[0,50],
                         y0=array([tt, rr1, u_r, theta_1, u_theta, phi1]),
                         args=(mm, qq, e, angular_mom),
                         vectorized=True,
                         method='RK45',
-                        atol=1e-7,
-                        rtol=1e-7,
-                        dense_output=True
+                        atol=1e-10,
+                        rtol=1e-10,
+                        dense_output=True,
+                        max_step=1e-3
                         )
         print(sol)
         result_spherical = vstack([sol.y[1], sol.y[3], sol.y[5]])
         plot_data(result_spherical, aax)
-        break
     plt.show()
 

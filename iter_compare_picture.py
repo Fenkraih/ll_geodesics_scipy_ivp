c0d7379cfc7f11becf2e973dd12747616ad4dff1
Altin <altin.shala@web.de>
2022-07-22 18:56:43 +0200

    For now final version of iterative comparison

--- iter_compare_picture.py
+++ iter_compare_picture.py
@@ -5,7 +5,7 @@ from initialconditions import lin_comb, space_light_timelike, check_tetrad, vart
 from decimal import Decimal
 from metric import q_metric
 import multiprocessing as mp
-from numpy import array, sin, cos, sum, pi, linspace, vstack, sqrt, tan, copy, set_printoptions, save, load
+from numpy import array, sin, cos, sum, pi, linspace, vstack, sqrt, tan, copy, set_printoptions, save, load, sort
 from scipy.integrate import solve_ivp, odeint
 from plotting import plot_data_3d
 from differential_equations import quadrupol_odeint, schwarzschild_odeint, quadrupol_ivp, schwarzschild_ivp
@@ -83,50 +83,152 @@ def generate_date():
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(lower_thing, upper_thing, 8)
     dark = [1, 1, 1, 0, 0, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[1], init_list[3], 8)
     dark = [1, 1, 1, 1, 1, 1, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[5], init_list[6], 8)
     dark = [1, 1, 1, 1, 0, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[3], init_list[4], 8)
     dark = [1, 1, 1, 1, 1, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[4], init_list[5], 8)
     dark = [1, 1, 1, 1, 1, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[4], init_list[5], 8)
     dark = [1, 1, 0, 0, 0, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[1], init_list[2], 8)
     dark = [1, 1, 0, 0, 0, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
     ii += 1
     dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{ii}/"
     init_list = linspace(init_list[1], init_list[2], 8)
     dark = [1, 1, 1, 1, 0, 0, 0, 0]
+    print(ii)
+    print(array(init_list[-1] - init_list[0]))
     # cald_this(init_list, mm, quad)
 
+
+def plotting_data():
+    fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(5, 5))
+
+    dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{1}/"
+    dark = [1, 1, 1, 0, 0, 0, 0, 0]
+    list_of_files = []
+    for elements in os.listdir(dirstring):
+        elements = elements.replace("r", "")
+        elements = elements.replace(".npy", "")
+        list_of_files.append(float(elements))
+    list_of_files = sort(list_of_files)
+    list_of_files = [str(x) for x in list_of_files]
+    #print(list_of_files)
+    for index_thing, file in enumerate(list_of_files):
+        result_spherical = load(dirstring + f"r{file}.npy")
+        plot_data_3d(result_spherical, ax[0], black_geodesic=dark[index_thing])
+
+    dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{3}/"
+    dark = [1, 1, 1, 1, 0, 0, 0, 0]
+    list_of_files = []
+    for elements in os.listdir(dirstring):
+        elements = elements.replace("r", "")
+        elements = elements.replace(".npy", "")
+        list_of_files.append(float(elements))
+    list_of_files = sort(list_of_files)
+    list_of_files = [str(x) for x in list_of_files]
+    #print(list_of_files)
+    for index_thing, file in enumerate(list_of_files):
+        result_spherical = load(dirstring + f"r{file}.npy")
+        plot_data_3d(result_spherical, ax[1], black_geodesic=dark[index_thing])
+
+    dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{5}/"
+    dark = [1, 1, 1, 1, 1, 0, 0, 0]
+    list_of_files = []
+    for elements in os.listdir(dirstring):
+        elements = elements.replace("r", "")
+        elements = elements.replace(".npy", "")
+        list_of_files.append(float(elements))
+    list_of_files = sort(list_of_files)
+    list_of_files = [str(x) for x in list_of_files]
+    #print(list_of_files)
+    for index_thing, file in enumerate(list_of_files):
+        result_spherical = load(dirstring + f"r{file}.npy")
+        plot_data_3d(result_spherical, ax[2], black_geodesic=dark[index_thing])
+
+    dirstring = f"/home/altin/ll_geod_scipy_ivp/data/it_{8}/"
+    dark = [1, 1, 1, 1, 0, 0, 0, 0]
+    list_of_files = []
+    for elements in os.listdir(dirstring):
+        elements = elements.replace("r", "")
+        elements = elements.replace(".npy", "")
+        list_of_files.append(float(elements))
+    list_of_files = sort(list_of_files)
+    list_of_files = [str(x) for x in list_of_files]
+    #print(list_of_files)
+    for index_thing, file in enumerate(list_of_files):
+        result_spherical = load(dirstring + f"r{file}.npy")
+        plot_data_3d(result_spherical, ax[3], black_geodesic=dark[index_thing])
+
+    for bx in ax:
+        xval = linspace(-2.1, 2.1, 1000)
+        yval = sqrt(2.1 ** 2 - xval ** 2)
+        bx.plot(xval, yval, "r")
+        bx.plot(xval, -yval, "r")
+
+        surface = 9
+        xval = linspace(-surface, surface, 1000)
+        yval = sqrt(surface ** 2 - xval ** 2)
+        bx.plot(xval, yval, "c")
+        bx.plot(xval, -yval, "c")
+
+    title_list = ["i=1, l≈1 e0", "i=3, l≈7 e-2", "i=5, l≈2 e-3", "i=8, l≈4 e-6"]
+    for kk, axis in enumerate(ax):
+        axis.set_title(title_list[kk], fontsize=20)
+        axis.tick_params(axis='both', which='major', labelsize=20)
+
+    for ax1 in ax.flat:
+        ax1.set(xlabel='x', ylabel='z')
+        ax1.xaxis.label.set_size(20)
+        ax1.yaxis.label.set_size(20)
+
+    for ax1 in ax.flat:
+        ax1.label_outer()
+
+    plt.show()
 
 if __name__ == "__main__":
     set_printoptions(precision=16)
@@ -134,7 +236,8 @@ if __name__ == "__main__":
     too_deep.terminal = True
     too_far.terminal = True
     plot_plane = "xz"
-
     quad = .1
     mm = 1
+    #generate_date()
+    plotting_data()
 
